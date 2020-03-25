8cf64e967686fb1e959b6d712e59189f894fbfd4
Deng <1592310935@qq.com>
2020-03-25 21:31:10 +0800

    删除

--- Test/target/Test/js/common.js
+++
@@ -1,5 +0,0 @@
-var cookie = {
-    getCookie: function (key) {
-        return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-    }
-};
